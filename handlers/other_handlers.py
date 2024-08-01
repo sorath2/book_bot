f790c92292817aaeaa71d62a45d269fa02255b47
Alex <sorath2@yandex.ru>
2024-08-01 21:37:11 +0500

    handlers

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,11 +1,9 @@
 from aiogram import Router
 from aiogram.types import Message
-from lexicon.lexicon import LEXICON_RU
 
 router = Router()
 
 
-# Хэндлер для сообщений, которые не попали в другие хэндлеры
 @router.message()
-async def send_answer(message: Message):
-    await message.answer(text=LEXICON_RU['other_answer'])
+async def send_echo(message: Message):
+    await message.answer(f'Это эхо! {message.text}')
